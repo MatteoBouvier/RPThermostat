7aa51e9537ea6b0e77e817872d1f176e8e32b530
MatteoBouvierVidium <m.bouvier@vidium-solutions.com>
2025-01-29 10:20:23 +0100

    feat: templates with for loops

--- rpthermostat/template.py
+++ rpthermostat/template.py
@@ -1,3 +1,4 @@
+from io import TextIOWrapper
 import re
 
 from udatetime import datetime  # pyright: ignore[reportImplicitRelativeImport, reportUnusedImport]
@@ -8,7 +9,8 @@ try:
 except ImportError:
     pass
 
-PATTERN = re.compile("{{(.*?)}}")
+PATTERN_EXPRESSION = re.compile("{{(.*?)}}")
+PATTERN_STATEMENT = re.compile("{%(.*?)%}")
 
 
 def _replace(variables: dict[str, Any]) -> Callable[[re.Match[str]], str]:
@@ -18,15 +20,78 @@ def _replace(variables: dict[str, Any]) -> Callable[[re.Match[str]], str]:
     return inner
 
 
+def _parse_for(
+    statement: str, line_offset: int, file: TextIOWrapper, variables: dict[str, Any]
+) -> tuple[str, int]:
+    if not statement.startswith("for "):
+        raise ValueError("Only for statements are allowed")
+
+    var, iterable = statement[4:].split(" in ")
+
+    parsed_for: list[str] = []
+
+    while True:
+        line = file.readline()
+
+        if not line:
+            raise ValueError("For statement was not closed, expected {% endfor %}")
+
+        else:
+            line = line.strip()
+
+        if line.replace(" ", "") == "{%endfor%}":
+            break
+
+        else:
+            parsed_for.append(line)
+
+    parsed = ""
+    line_nb = 1
+
+    for it in variables[iterable]:
+        for line_nb, line in enumerate(parsed_for, start=1):
+            parsed += _parse_expression(
+                line, line_nb + line_offset, variables | {var: it}
+            )[0]
+
+    return parsed, line_offset + line_nb
+
+
+def _parse_expression(
+    line: str, line_nb: int, variables: dict[str, Any]
+) -> tuple[str, int]:
+    try:
+        return PATTERN_EXPRESSION.sub(_replace(variables), line), line_nb + 1
+
+    except NameError as e:
+        raise NameError(f"Template formatting failed at line #{line_nb}\n{line}\n{e}")
+
+
 def parse(path: str, **variables: Any) -> str:
     parsed = ""
+    line_nb = 1
 
     with open(path, "r") as file:
-        for i, line in enumerate(file, start=1):
-            try:
-                parsed += PATTERN.sub(_replace(variables), line)
+        while True:
+            line = file.readline()
+            if not line:
+                break
 
-            except NameError as e:
-                raise NameError(f"Template formatting failed at line #{i}\n{line}\n{e}")
+            else:
+                line = line.strip()
+
+            statement_match = PATTERN_STATEMENT.search(line)
+
+            if statement_match is not None:
+                statement = line[
+                    statement_match.start() + 2 : statement_match.end() - 2
+                ].strip()
+
+                parsed_for, line_nb = _parse_for(statement, line_nb, file, variables)
+                parsed += parsed_for
+
+            else:
+                parsed_expr, line_nb = _parse_expression(line, line_nb, variables)
+                parsed += parsed_expr
 
     return parsed
